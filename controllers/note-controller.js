ff4a8c168b7b56a97687d98019189c0107c56b12
Sophie Gill <sophie-gill@hotmail.com>
2016-08-23 18:16:46 +0100

    attempting to add a note

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -1,23 +1,43 @@
+var noteList = new NoteList();
+noteList.addNote("This is a note for views testing");
+// noteList.addNote("Another note for testing linking between notes");
+// noteList.addNote("Yet another note");
+var noteView = new NoteListView(noteList);
+
+
 document.addEventListener("DOMContentLoaded", function () {
 
   (function renderView() {
 
-    var noteList = new NoteList();
-    noteList.addNote("This is a note for views testing");
-    // noteList.addNote("Another note for testing linking between notes");
-    // noteList.addNote("Yet another note");
-    var noteView = new NoteListView(noteList);
-    htmlCode = noteView.render();
 
     window.addEventListener("load", function() {
-      (function loadNoteView() {
-        document.getElementById("app").innerHTML = htmlCode;
-      })();
+      loadNoteView();
+      document.getElementById('save-note-button').addEventListener("click", function() {
+        console.log(1);
+        note = document.getElementById("noteContent").value;
+        console.log(note);
+        noteList.addNote(note);
+        loadNoteView();
+      });
     });
+
+    document.getElementById('save-note-button').addEventListener("click", function() {
+      console.log(1);
+      note = document.getElementById("noteContent").value;
+      console.log(note);
+      noteList.addNote(note);
+      loadNoteView();
+    });
+
 
     window.addEventListener("hashchange", function() {
       showSelectedNote();
     });
+
+    function loadNoteView() {
+      var htmlCode = noteView.render();
+      document.getElementById("app").innerHTML = document.getElementById("app").innerHTML + htmlCode;
+    }
 
     function showSelectedNote() {
        showNote(getNoteID(window.location));
